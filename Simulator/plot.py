540c5f2969e75a0f461e9d46090cfe8d92c53b00
Aayush Kapadia <kapadiaaayush@gmail.com>
2017-05-05 17:02:46 +0530

    Remove history name error for absolute paths

--- Simulator/plot.py
+++ Simulator/plot.py
@@ -2,9 +2,22 @@ from Simulator import *
 import XMLParser
 import textToXML
 
+
 def getHistoryFileName(xmlFileName):
 	y = xmlFileName[:-3]
-	return 'history_' + y + 'txt'
+	y = y + 'txt'
+
+	i = len(y) - 1
+	while i>=0 :
+		if y[i]=='\\' or y[i]=='/' :
+			break
+		i-=1
+
+	if i>=0 :
+		return y[:i+1] + 'history_' + y[i+1:]
+	else:
+		return 'history_' + y
+	
 
 def plotFromXML(fileName,simulationTime,chemicalList):
 	historyFile = getHistoryFileName(fileName)
